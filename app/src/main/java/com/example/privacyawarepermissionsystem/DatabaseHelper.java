8e95ac70d1b55fb6be3519259c568440c7d7deec
William Xu <xuweize2924@gmail.com>
2026-05-25 00:53:32 -0700

    Added SQLite DatabaseHelper

---
+++ app/src/main/java/com/example/privacyawarepermissionsystem/DatabaseHelper.java
@@ -0,0 +1,26 @@
+package com.example.privacyawarepermissionsystem;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "privacy.db";
+    private static final int DB_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE apps (id INTEGER PRIMARY KEY, appName TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS apps");
+        onCreate(db);
+    }
+}
